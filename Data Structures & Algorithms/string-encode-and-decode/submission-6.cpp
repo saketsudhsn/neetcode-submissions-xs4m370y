39f07b44ee63bbae5257917630100ef03fc717d1
saketsudhsn <47291012+saketsudhsn@users.noreply.github.com>
2026-04-10 14:47:06 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) 
+    {
+        std::string outStr;
+        for(auto str : strs)
+        {
+            outStr += std::to_string(str.size()) + "#" + str;
+        }
+        std::cout << "outStr: " << outStr << "\n";
+        return outStr;
+    }
+
+    vector<string> decode(string s) 
+    {
+        std::vector<std::string> strs;
+
+        int i = 0;
+        while(i < s.size())
+        {
+            int numLength{};
+            while(s[i] != '#')
+            {
+                i++;
+                numLength++;
+            }
+
+            int size = stoi(s.substr(i-numLength, i));
+            std::cout << "size: " << size << "\n";
+            strs.push_back(s.substr(i+1, size));
+            i += size+1;
+        }
+
+        return strs;
+
+    }
+};
