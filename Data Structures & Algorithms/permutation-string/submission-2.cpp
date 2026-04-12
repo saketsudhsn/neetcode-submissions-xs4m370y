7d770977f95b90d28075e7abef250c43d09721c8
saketsudhsn <47291012+saketsudhsn@users.noreply.github.com>
2026-04-13 04:02:09 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    std::string getWindowFreqStr(const std::string& str)
+    {
+        std::vector<int> windowCharFreq(26, 0);
+        for(int i = 0; i < str.size(); i++)
+        {
+            windowCharFreq[str[i]-'a']++;
+        }
+
+        std::string sWindowCharFreq;
+        for(int i = 0; i < 26; i++)
+        {
+            sWindowCharFreq.append(std::to_string(windowCharFreq[i]));
+        }
+        
+        return sWindowCharFreq;
+    }
+
+    bool checkInclusion(string s1, string s2) 
+    {
+        int windowSize = s1.size();
+        if(s2.size() < windowSize)
+            return false;
+        else if(s1 == s2)
+            return true;
+        
+        std::unordered_set<std::string> windowFreqMap;
+        windowFreqMap.insert(getWindowFreqStr(s1));
+
+        int startIdx = 0;
+        int lastIdx = windowSize - 1;
+        while(lastIdx < s2.size())
+        {
+            std::string currStrWindow(s2.substr(startIdx, windowSize));
+            if(windowFreqMap.contains(getWindowFreqStr(currStrWindow)))
+                return true;
+            
+            startIdx++;
+            lastIdx++;
+        }
+        
+        return false;
+    }
+};
