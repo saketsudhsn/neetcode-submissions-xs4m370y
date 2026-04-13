1d52fc70910d91303e19a01fea116ace340a4958
saketsudhsn <47291012+saketsudhsn@users.noreply.github.com>
2026-04-13 15:01:26 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+         char charStack[s.size()];
+        int stackLastIndex = 0;
+        charStack[stackLastIndex] = s[0];
+        
+        int i = 1;
+        while(i < s.size())
+        {
+            if(stackLastIndex == -1 && (s[i] == ')' || s[i] == '}' || s[i] == ']'))
+                return false;
+
+            if(s[i] == ')')
+            {
+                if(charStack[stackLastIndex] == '(')
+                    stackLastIndex--;
+                else
+                    return false;
+            }
+            else if(s[i] == '}')
+            {
+                if(charStack[stackLastIndex] == '{')
+                    stackLastIndex--;
+                else
+                    return false;
+            }
+            else if(s[i] == ']')
+            {
+                if(charStack[stackLastIndex] == '[')
+                    stackLastIndex--;
+                else
+                    return false;
+            }
+            else
+                charStack[++stackLastIndex] = s[i];
+
+            i++;
+        }
+
+        if(stackLastIndex == -1)
+            return true;
+        else
+            return false;   
+    }
+};
