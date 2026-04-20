d8fc84f9d377ac78fbd35800c50b6bb57b9e0e06
saketsudhsn <47291012+saketsudhsn@users.noreply.github.com>
2026-04-20 18:41:04 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+        auto cmp = [](std::vector<int> point1, std::vector<int> point2)->bool
+        {
+            return (point1[0] * point1[0]) + (point1[1] * point1[1]) < (point2[0] * point2[0]) + (point2[1] * point2[1]);
+        };
+
+        std::priority_queue<std::vector<int>, std::vector<std::vector<int>>, decltype(cmp)> pQueue(cmp);
+        for(auto point : points)
+        {
+            pQueue.push(point);
+            if(pQueue.size() > k)
+                pQueue.pop();
+        }
+
+        std::vector<std::vector<int>> res;
+        while(pQueue.size())
+        {
+            res.push_back(pQueue.top());
+            pQueue.pop();
+        }
+
+        return res;
+    }
+};
