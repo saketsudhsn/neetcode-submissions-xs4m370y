bcdd251df7872f9e912d4ca00cfb7788a9a12838
saketsudhsn <47291012+saketsudhsn@users.noreply.github.com>
2026-04-21 04:29:14 +0530

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.cpp
@@ -0,0 +1,53 @@
+class MedianFinder {
+private:
+    
+    //minHeap 6 7 8 9 10
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    // maxHeap 5 4 3 2 1
+    std::priority_queue<int> maxHeap;
+
+public:
+
+    MedianFinder() 
+    {
+    }
+    
+    void addNum(int num) 
+    {
+        if(minHeap.empty() || num > minHeap.top())
+        {
+            std::cout << "min heap push: " << num << "\n";
+            minHeap.push(num);
+        }
+        else
+        {
+            std::cout << "max heap push: " << num << "\n";
+            maxHeap.push(num);
+        }
+
+        if(minHeap.size() > maxHeap.size() + 1)
+        {
+            int num = minHeap.top();
+            minHeap.pop();
+            maxHeap.push(num);
+        }
+        else if(minHeap.size() + 1 < maxHeap.size())
+        {
+            int num = maxHeap.top();
+            maxHeap.pop();
+            minHeap.push(num);
+        }
+    }
+    
+    double findMedian() 
+    {
+        if(minHeap.size() == maxHeap.size())
+            return ((minHeap.top() + maxHeap.top())/2.0);
+        else if(minHeap.size() > maxHeap.size())
+            return minHeap.top();
+        else
+            return maxHeap.top();
+    }
+};
+
+
