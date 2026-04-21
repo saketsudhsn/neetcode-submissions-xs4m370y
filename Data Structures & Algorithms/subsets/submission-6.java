fb69e3beb56034674482d6ec17eabe6c8ce3567a
saketsudhsn <47291012+saketsudhsn@users.noreply.github.com>
2026-04-22 00:42:15 +0530

    Add: subsets - submission-6

---
+++ Data Structures & Algorithms/subsets/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    private void subsets(int[] nums, int index, List<Integer> list, List<List<Integer>> answer) {
+
+        for (int idx = index; idx < nums.length; idx++) {
+            list.add(nums[idx]);
+            answer.add(new ArrayList<>(list));
+            subsets(nums, idx + 1, list, answer);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> answer = new ArrayList<>();
+
+        answer.add(new ArrayList<>());
+        subsets(nums, 0, new ArrayList<>(), answer);
+        return answer;
+    }
+}
